54cbe253e26acfcaac9eadb70dabf9db205c5ce4
Julesmcghouls <132119772+Julesmcghouls@users.noreply.github.com>
2023-11-09 23:27:34 -0600

    Task 2 pt.2 - Finished SQL Task

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,26 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+//Add @Entity annotation
+@Entity
 public class Employer extends AbstractEntity {
+    //Add the field for location with validation that ensures it is not empty and has a reasonable length
+    @NotNull
+    @Size(min = 2, max = 100)
+    private String location;
+
+    //Add no arg constructor for hibernate to create an object
+    public Employer() {}
+
+    //public accessor methods w/ getter and setter for location
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
